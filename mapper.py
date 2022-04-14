959565b8a0c2501a37792cad00e3c814ec00ac72
cody vernon <cody.vernon@wsu.edu>
2022-04-13 20:54:58 -0700

    finished mapper and reducer implementation

--- mapper.py
+++ mapper.py
@@ -1,7 +1,17 @@
 #!/bin/python3
 
+import sys
+
 def main():
-    pass
+    # get each line
+    for line in sys.stdin:
+        # get rid of \n and split the words
+        line = line.strip().split()
+
+        # if the line is valid
+        if line[3] == 'T':
+            # print first 3 terms to stdout
+            print(" ".join(line[:3]))
 
 if __name__ == '__main__':
     main()
--- reducer.py
+++ reducer.py
@@ -1,7 +1,39 @@
 #!/bin/python3
 
+import sys
+
 def main():
-    pass
+
+    curPage = None
+    pageCount = 0
+    pageSize = 0
+
+    for line in sys.stdin:
+
+        line = line.strip().split() # remove newlines and split the line
+        page = line[0] # grab the page name
+        count = int(line[1]) # grab the count
+        size = int(line[2]) # grab the size
+
+        # add to count and size if we get a same page
+        if page == curPage:
+            pageCount += count
+            pageSize += size
+        else:
+            # otherwise, print out the count of the current page
+            if curPage:
+                print(f'{curPage}' + '{visits} ' + str(pageCount))
+                print(f'{curPage}' + '{size} ' + str(pageSize))
+
+            # update the page, count, and size
+            pageCount = count
+            pageSize = size
+            curPage = page
+
+    # print out the final page seen
+    if curPage == page:
+        print(f'{curPage}' + '{visits} ' + str(pageCount))
+        print(f'{curPage}' + '{size} ' + str(pageSize))
 
 if __name__ == '__main__':
     main()
